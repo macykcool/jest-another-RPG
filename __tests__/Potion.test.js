cdc830b01ec7a6cea8a09ee0fb1e666a755f115b
Macy Kay Mannix <macymannix@gmail.com>
2022-05-17 18:29:00 -0700

    1034

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,3 +1,4 @@
+const Player = require('../lib/player');
 const Potion = require('../lib/Potion');
 test('creates a random potion object', () => {
     const potion = new Potion();
@@ -5,5 +6,13 @@ test('creates a random potion object', () => {
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0); 
     expect(potion.value).toEqual(expect.any(Number));
+});
+
+test("get player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
 });
 
