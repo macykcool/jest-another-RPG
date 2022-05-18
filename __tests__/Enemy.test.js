27c47f022f72173a21d68ff776abf241a8b63d9d
Macy Kay Mannix <macymannix@gmail.com>
2022-05-17 18:47:48 -0700

    1035

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,19 @@
+// // const { jest } = require('@jest/globals');
+// const exp = require('constants');
+// const { expect } = require('expect');
+// const { test } = require('picomatch');
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
